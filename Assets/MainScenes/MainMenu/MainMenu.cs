61afd9421fe743344beee14fa87136ccfce4de97
MICHAËL TREMBLAY <mtrembl233@etu.uqac.ca>
2025-02-28 18:17:04 -0500

    Add base de main menu, les bouttons marchent, reste a entrer le nom des bonnes scenes

---
+++ Assets/MainScenes/MainMenu/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+    public void GuideScene()
+    {
+       // SceneManager.LoadSceneAsync("OptionScene");
+    }
+    public void CreditsScene()
+    {
+        // SceneManager.LoadSceneAsync("CreditsScene");
+    }
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        QuitGame();
+    }
+    
+
+}
