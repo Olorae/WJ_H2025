54080c0f88e446bb4cef2f45bdf62b0f5781f746
Catherine Bergeron <bergeron.catherine@icloud.com>
2025-03-01 16:54:35 -0500

    Add a lot of assets and start working on enemy animation

--- Assets/Scripts/UI/InsanityBar.cs
+++ Assets/Scripts/UI/InsanityBar.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 public class InsanityBar : MonoBehaviour
 {
     public GameObject thisUI;
+
+    
+    public Sprite BossBar;
+
+    public Image background;
+
+    public Image fill;
+
+    public Image Skull;
     // Start is called before the first frame update
     void Start()
     {
         thisUI.SetActive(false);
-        
+        FindObjectOfType<SpawnManager>().BossSpawned += bossSpawned;
     }
 
     // Update is called once per frame
@@ -21,6 +30,13 @@ public class InsanityBar : MonoBehaviour
         
     }
 
+    public void bossSpawned()
+    {
+        
+       background.color = Color.gray;
+       fill.color = Color.red;
+       Skull.color = Color.red;
+    }
     public void UpdateValue(float newValue)
     {
         
